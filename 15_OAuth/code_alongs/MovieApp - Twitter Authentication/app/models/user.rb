a2bf9717e18c41f23def53ace024f2975aed4b74
Ben Askins <ben.askins@gmail.com>
2013-08-12 20:35:35 +1000

    Lesson 15

---
+++ 15_OAuth/code_alongs/MovieApp - Twitter Authentication/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  has_many :reviews
+  
+  def self.from_omniauth(auth_hash)
+    criteria = auth_hash.slice(:provider, :uid)
+
+    where(criteria).first_or_initialize.tap do |user|   
+      user.uid = auth_hash.uid                                                     
+      user.provider = auth_hash.provider                                           
+      user.username = auth_hash.info.nickname                                      
+      user.oauth_token = auth_hash.credentials.token                               
+      user.oauth_secret = auth_hash.credentials.secret                             
+      user.save                                                                    
+    end                                                                            
+  end
+end
