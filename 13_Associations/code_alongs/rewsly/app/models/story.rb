214e2f8b80f67892d7f87b9aadb87953918a045a
Ben Askins <ben.askins@gmail.com>
2013-08-14 20:25:44 +1000

    Cleanup

--- 13_Associations/code_alongs/rewsly/app/models/story.rb
+++ 13_Associations/code_alongs/rewsly/app/models/story.rb
@@ -1,6 +1,8 @@
 class Story < ActiveRecord::Base
+  attr_accessor :category
+
   validates :headline, :content, :presence => true
   
   belongs_to :user
-  has_many :comments
+  has_many :comments  
 end
--- 13_Associations/code_alongs/rewsly/test/models/story_test.rb
+++ 13_Associations/code_alongs/rewsly/test/models/story_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "that stories can have categories" do
+    story = Story.new
+    story.category = "arts"
+    assert_equal "arts", story.category
+  end
+  
+  
+  test "that we can save a category" do
+    story = Story.create!(:headline => "_", :content => "_", :category => "arts")
+    
+    assert_equal "arts", Story.find(story.id).category
+  end
+
 end
