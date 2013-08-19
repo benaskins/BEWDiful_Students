57de9a375d591d4877b8e7f25a327e781869264b
Ben Askins <ben.askins@gmail.com>
2013-08-19 17:09:34 +1000

    New rails app for carrierwave demo

---
+++ 17_Gems/code_alongs/profile_pics_ba/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProfilePicsBa::Application.config.secret_key_base = 'c9eecbf7a754f429e172c1a18bde5bf22fa5374f8f4ff0dbe2302351cb4ee325b9081f7b444279d4472f3bebaf860083705799ab105e77fc465656fdb4247ae6'
---
+++ 17_Gems/code_alongs/profile_pics_ba/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProfilePicsBa::Application.config.session_store :cookie_store, key: '_profile_pics_ba_session'
